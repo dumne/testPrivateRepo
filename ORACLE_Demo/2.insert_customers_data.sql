37079bd7cfb0ce77fa46a1d2fdb7939d221ae040
dumne <dumnesreenivas@gmail.com>
2019-04-10 01:58:54 +0530

    Update 2.insert_customers_data.sql

--- ORACLE_Demo/2.insert_customers_data.sql
+++ ORACLE_Demo/2.insert_customers_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customers2 (customer_id , customer_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
-INSERT INTO Customers2 (customer_id , customer_name, city) VALUES (2, 'Kalaiselvi', 'Bothell');
+INSERT INTO customers (customer_id , customer_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
+INSERT INTO customers (customer_id , customer_name, city) VALUES (2, 'Kalaiselvi', 'Bothell');
