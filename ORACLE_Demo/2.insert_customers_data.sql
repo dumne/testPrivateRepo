b490f0cdc92f665f8072f4868e66b3654bbdf957
dumne <dumnesreenivas@gmail.com>
2019-04-16 19:56:18 +0530

    Delete 2.insert_customers_data.sql

--- ORACLE_Demo/2.insert_customers_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customers (customer_id , customer_name, city) VALUES (1, 'Tom B. Erichsen', 'Bellevue');
-INSERT INTO customers (customer_id , customer_name, city) VALUES (2, 'Kalaiselvi', 'Bothell');
