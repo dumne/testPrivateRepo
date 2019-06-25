8729d403f8ed4e9a8fff499ba3bedc8f97db8ef6
dumne <dumnesreenivas@gmail.com>
2019-06-25 20:11:17 +0530

    Update 1.create_customers_table.sql

--- oracle/1.create_customers_table.sql
+++ oracle/1.create_customers_table.sql
@@ -1,8 +1,8 @@
-SET ECHO ON;
+spool out.txt;
 CREATE TABLE Customers
 ( user_id number(10) NOT NULL,  
   user_name varchar2(50) NOT NULL,  
   city varchar2(50),
   state1 varchar2(50)   
 );  
-EXIT;
+spool off;
