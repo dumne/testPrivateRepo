61d487a66cf9ae93fa521b93fed453e2fff75907
dumne <dumnesreenivas@gmail.com>
2019-06-27 13:49:26 +0530

    Update patch_20_0_0_14_create_item_table.sql

--- oracle/patch_20_0_0_14_create_item_table.sql
+++ oracle/patch_20_0_0_14_create_item_table.sql
@@ -1,6 +1,1 @@
-CREATE TABLE Item
-( user_id number(10) NOT NULL,  
-  user_name varchar2(50) NOT NULL,  
-  city varchar2(50),
-  state1 varchar2(50)   
-);
+CREATE TABLE Item ( id number(10) NOT NULL,  name varchar2(50) NOT NULL);
